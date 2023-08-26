6c2b32da604d6232fafd4563fd6b94ab57c06f45
DennisD2 <info@spurtikus.de>
2023-08-26 19:00:22 +0200

    Added missing file

---
+++ all2deposit/types.go
@@ -0,0 +1,50 @@
+package main
+
+import "strings"
+
+// ConversionInfo keeps config info for conversion
+type ConversionInfo struct {
+	inFile           string
+	outFile          string
+	dataAlign        int
+	text             int
+	vector           string
+	dryMode          bool
+	debug            int
+	outContent       strings.Builder
+	outFormat        OutFormat
+	state            ConversionState
+	directiveBuf     []int
+	emitStartAddress int
+	emitInfo         []EmitInfo
+	directive        Directive
+	lineNumberPos    int
+}
+
+// OutFormat number format to write
+type OutFormat int
+
+const (
+	Octal OutFormat = 0
+)
+
+// OutFormat number format to write
+type ConversionState int
+
+const (
+	Start ConversionState = 0
+)
+
+// Directive types
+type Directive int
+
+const (
+	None Directive = 0
+)
+
+type EmitInfo struct {
+	address int
+	value   int
+}
+
+const LF byte = 0xa
