3d9501bdfea62e45a36b412a3d1015eed22cfe01
DennisD2 <info@spurtikus.de>
2023-09-15 15:52:14 +0200

    Improved example

--- as-clang-invocation/use_c_fun.s
+++ as-clang-invocation/use_c_fun.s
@@ -3,6 +3,7 @@
 
         .globl _addasm
         .extern _printLine      # clang function declared as extern
+        .extern _add      # clang function declared as extern
 
         .text
 
@@ -11,7 +12,13 @@ _addasm:
         clr     r0
         add     2(sp),r0
         add     4(sp),r0
-        jsr     pc,_printLine   # calling the clang function
+        jsr     pc,_printLine      # calling the clang printLine() function
+
+        mov     $20,-(sp)          # push literal 20 dec. to stack
+        mov     $30,-(sp)          # push literal 30 dec. to stack
+        jsr     pc,_add            # calling the clang add() function
+        add     $4,sp              # fix stack pointer, i.e. remove parameters 20 and 30 from stack
+
         rts     pc
 
         .end
